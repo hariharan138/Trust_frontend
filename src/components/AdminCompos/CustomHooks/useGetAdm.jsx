af00b9f0274919ac1cb5d0a1a90e948954755a5d
HariHaran <101501297+hariharan138@users.noreply.github.com>
2025-04-20 03:25:09 +0530

    Update useGetAdm.jsx

--- src/components/AdminCompos/CustomHooks/useGetAdm.jsx
+++ src/components/AdminCompos/CustomHooks/useGetAdm.jsx
@@ -1,30 +1,30 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+// import axios from 'axios'
+// import React, { useEffect, useState } from 'react'
 
-const useGetAdm = (url) => {
+// const useGetAdm = (url) => {
 
-    const [apidata, setApidata] = useState([])
+//     const [apidata, setApidata] = useState([])
 
-    let getApi = async ()=>{
-        try{
-            let {data} = await axios.get(url, {
-                withCredentials: true
-            })
-        if(data?.data.length>0){
-            setApidata(data.data)
-        }
-        }
-        catch(err){
-            console.error('Error Response:', err.response?.data); 
-        }
+//     let getApi = async ()=>{
+//         try{
+//             let {data} = await axios.get(url, {
+//                 withCredentials: true
+//             })
+//         if(data?.data.length>0){
+//             setApidata(data.data)
+//         }
+//         }
+//         catch(err){
+//             console.error('Error Response:', err.response?.data); 
+//         }
         
-    }
+//     }
 
-    useEffect(()=>{
-        getApi()
-    }, [])
+//     useEffect(()=>{
+//         getApi()
+//     }, [])
 
-  return [apidata, setApidata]
-}
+//   return [apidata, setApidata]
+// }
 
-export default useGetAdm
+// export default useGetAdm
